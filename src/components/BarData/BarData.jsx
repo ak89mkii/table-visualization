f324a30ad21c39167bec786ecacc7c8f4df5242b
Alex Kasem <spock-117@Alexs-MacBook-Pro.local>
2022-03-27 19:01:39 -0400

    Added Responsivecontainer to components.

--- src/components/BarData/BarData.jsx
+++ src/components/BarData/BarData.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import '../../App.css';
-import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip, Legend  } from 'recharts';
+import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer  } from 'recharts';
 
-const PieData = (props) => {
+const BarData = (props) => {
     return (
-        <div>
-            <Container>
-                <h5>Bar Graph: Coffee Stock | Sold</h5>
+        <Container> 
+            <h5>Bar Graph: Coffee Stock | Sold</h5>
+            <div className='chartContainer'>
+            <ResponsiveContainer>
                 <BarChart width={1000} height={600} data={props.infos}>
                     <CartesianGrid stroke="#ccc" />
                     <XAxis dataKey="name" />
@@ -17,9 +18,10 @@ const PieData = (props) => {
                     <Bar dataKey="stock" fill="orange" />
                     <Bar dataKey="sold" fill="#0047ab" />
                 </BarChart>
-            </Container>
-        </div>
+            </ResponsiveContainer>
+            </div>
+         </Container>
     )
 }
 
-export default PieData;
+export default BarData;
